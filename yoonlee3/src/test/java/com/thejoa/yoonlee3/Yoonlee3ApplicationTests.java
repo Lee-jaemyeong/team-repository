d2e468e0865ce0004925e308aa9209676aa9024a
Lee-jaemyeong <kinglee98@naver.com>
2025-04-14 11:20:51 +0900

    2025-04-14 (1) diary_project init

---
+++ yoonlee3/src/test/java/com/thejoa/yoonlee3/Yoonlee3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejoa.yoonlee3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yoonlee3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
