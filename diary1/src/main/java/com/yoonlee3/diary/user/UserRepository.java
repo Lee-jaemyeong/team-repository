1a38f68578b0bde7f9c4632d251bc05fde80d613
Lee-jaemyeong <kinglee98@naver.com>
2025-04-18 20:51:15 +0900

    2025-04-18 병합충돌

---
+++ diary1/src/main/java/com/yoonlee3/diary/user/UserRepository.java
@@ -0,0 +1,27 @@
+package com.yoonlee3.diary.user;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User,Long> {  
+	   // 2. read
+	   Optional<User> findByEmail(String email);
+	   
+	   @Query("select u from User u where u.username= :username") User findByUsername(String username);
+	   
+	   // 3. update
+	   @Modifying
+	   @Transactional
+	   @Query ("update User u set u.username= :username where u.user_id= :user_id ")
+	   int updateById( Long user_id , String username);
+	   
+	   @Modifying
+	   @Transactional
+	   @Query ("update User u set u.password= :password where u.email= :email ")
+	   int updateByIdAndPassword( String password , String email);
+}
---
+++ diary1/src/main/java/com/yoonlee3/diary/user/UserService.java
@@ -0,0 +1,41 @@
+package com.yoonlee3.diary.user;
+
+import java.util.List;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	
+	//insert
+	public User insertUser(User user) { 
+		user.setPassword(passwordEncoder.encode( user.getPassword() ));
+		return userRepository.save(user);
+	}
+	//selectAll
+	public List<User> selectUserAll(){
+		return userRepository.findAll();
+	}
+	
+	//select
+	public User selectUser(Long user_id) {
+		return userRepository.findById(user_id).get();
+	}
+	
+	//update
+	public int updateByPass( User user ) {
+		return userRepository.updateByIdAndPassword(user.getPassword(), user.getEmail());
+	}
+	public int updateByUsername(Long user_id, User user ) {
+		return userRepository.updateById(user.getUser_id(), user.getUsername());
+	}	
+	
+}
