68b0c9a11c604285b158be69ef8451070ec98bb4
Lee-jaemyeong <kinglee98@naver.com>
2025-07-25 16:09:51 +0900

    Update ProfileController.java

--- diary/src/main/java/com/yoonlee3/diary/user/ProfileController.java
+++ diary/src/main/java/com/yoonlee3/diary/user/ProfileController.java
@@ -32,27 +32,24 @@ public class ProfileController {
         User targetUser = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
 
-        // principal이 null인지 확인
         User viewer = null;
         if (principal != null) {
             viewer = userRepository.findByUsername(principal.getName());
         }
 
-        // 로그인된 사용자가 있고, targetUser와 차단 관계가 있으면 차단 여부 확인
+        // 차단 여부 확인
         boolean isBlocked = false;
         if (viewer != null) {
-            // 차단 여부 확인
             isBlocked = blockService.isBlocked(viewer, targetUser);
         }
         
-        // 프로필 정보 가져오기
         UserProfileDto userProfile = followService.getUserProfile(viewer, targetUser);
 
         // 모델에 사용자 프로필 정보 추가
         model.addAttribute("userProfile", userProfile);
-        model.addAttribute("viewer", viewer);  // 팔로우 여부 확인
+        model.addAttribute("viewer", viewer);
 
-        return "profile";  // Thymeleaf 템플릿 이름 반환
+        return "profile"; 
     }
     
 }
