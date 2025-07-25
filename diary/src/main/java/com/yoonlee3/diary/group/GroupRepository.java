c7d4a27e642ffbcd5f8e2f7c75c1e3a62f1eaad0
Lee-jaemyeong <kinglee98@naver.com>
2025-07-25 15:59:37 +0900

    Update GroupRepository.java

--- diary/src/main/java/com/yoonlee3/diary/group/GroupRepository.java
+++ diary/src/main/java/com/yoonlee3/diary/group/GroupRepository.java
@@ -10,17 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 public interface GroupRepository extends JpaRepository<YL3Group, Long> {
 
-	// Group
-	// C
-
-	// R
 	@Query("select g from YL3Group g where g.group_title = :group_title")
 	YL3Group findByGroupTitle(String group_title);
 
 	@Query("select g from YL3Group g where g.group_leader = :user_id")
 	List<YL3Group> findByGroupLeader(Long user_id);
 
-	// U
 	@Modifying
 	@Transactional
 	@Query("update YL3Group g set g.badge.id = g.badge.id +1 where g.id = :group_id")
@@ -32,7 +27,6 @@ public interface GroupRepository extends JpaRepository<YL3Group, Long> {
 			+ " where g.id = :group_id")
 	int updateGroup(String group_title, String group_content, Long group_id);
 
-	// D
 	@Modifying
 	@Transactional
 	@Query("delete from YL3Group g where g.id = :group_id and g.group_leader.id = :user_id")
