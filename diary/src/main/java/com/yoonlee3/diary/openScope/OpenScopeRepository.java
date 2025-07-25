bc3caac85e6a4994d064ff2efb397eec7440348d
Lee-jaemyeong <kinglee98@naver.com>
2025-07-25 16:05:53 +0900

    Update OpenScopeRepository.java

--- diary/src/main/java/com/yoonlee3/diary/openScope/OpenScopeRepository.java
+++ diary/src/main/java/com/yoonlee3/diary/openScope/OpenScopeRepository.java
@@ -5,13 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface OpenScopeRepository extends JpaRepository<OpenScope, Long> {
 
-	// C
-
-	// R
 	@Query("select os from OpenScope os where os.id = :open_scope_id")
 	OpenScope findByOpenScopeId(Long open_scope_id);
 
-	// U
-
-	// D
 }
